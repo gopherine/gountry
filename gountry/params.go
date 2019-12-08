c1213f1779aa562f94cfbdeddb0e45d445a7017c
Atharva <atharvajava@gmail.com>
2019-12-08 21:40:09 +0100

    gountry initial release

--- gountry/params.go
+++
@@ -1,9 +0,0 @@
-package gountry
-
-import "github.com/gopherine/Gountry/model"
-
-//Country - Optional Parameter Types
-func Country(c *model.CountryType) error {
-	c.Country = "India"
-	return nil
-}
